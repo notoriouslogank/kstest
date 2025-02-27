69f61c382fdd18475015e65c9a9adb6fe3ca04e1
logank <logan.aker1989@gmail.com>
2025-02-26 22:57:45 -0500

    hm

--- sender.py
+++ sender.py
@@ -1,11 +1,10 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/tty1', baudrate=115200, bytesize=8, write_timeout=10)
+ser = serial.Serial('/dev/tty1', baudrate=115200, bytesize=8, write_timeout=10, timeout=30)
 
 def send_data(data):
     if ser.is_open:
-        data = data + '\n'
         ser.write(data.encode('utf-8'))
         ser.flush()
         print(f'Send: {data}')
@@ -17,7 +16,7 @@ if __name__ == "__main__":
     try:
         while True:
             msg = input("Message: ")
-            send_data(msg.encode('utf-8'))
+            send_data(msg)
             time.sleep(0.1)
     except KeyboardInterrupt:
         print("Stopping...")
