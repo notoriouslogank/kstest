45d682a7251da775710af68e31648f282bd7181c
logank <logan.aker1989@gmail.com>
2025-02-26 22:19:34 -0500

    trying to make this work

--- sender.py
+++ sender.py
@@ -5,7 +5,9 @@ ser = serial.Serial('/dev/tty1', baudrate=115200, timeout=1)
 
 def send_data(data):
     if ser.is_open:
+        data = data + '\n'
         ser.write(data.encode('utf-8'))
+        ser.flush()
         print(f'Send: {data}')
     else:
         print("Port not open.")
